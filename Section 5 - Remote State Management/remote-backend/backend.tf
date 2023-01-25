b298ab470e8112d8d49c2974b8a7dcb904af66c8
chelviso <111324611+chelviso@users.noreply.github.com>
2023-01-24 21:33:27 -0600

    Update backend.tf

--- Section 5 - Remote State Management/remote-backend/backend.tf
+++ Section 5 - Remote State Management/remote-backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kplabs-terraform-backend"
-    key    = "network/terraform.tfstate"
+    key    = "network/terraform.tfstate"  #This is the path or subdirectory where the statefile will be stored
     region = "us-east-1"
   }
 }
